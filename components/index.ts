52d929729381d2edeb4c42d3b60ce030fab12ecf
Miraj Mainali <merazzmainali05@gmail.com>
2024-01-06 11:34:49 +1100

    added the searchManufacturer component

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import Hero from "./Hero";
-import CustomButton from "./CustomButton"; 
+import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import Searchbar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton,Navbar,Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  CustomFilter,
+  Searchbar,
+  SearchManufacturer,
+};
