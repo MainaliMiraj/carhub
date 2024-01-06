69e543b16b2b85c1428e91569836c99b7ba47b20
Miraj Mainali <merazzmainali05@gmail.com>
2024-01-07 00:15:37 +1100

    added the custom filter for the cars and created reuseable searchParams util

--- types/index.ts
+++ types/index.ts
@@ -5,9 +5,9 @@ export interface CustomButtonProps {
   containerStyle?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
-  textStyles?:string,
-  rightIcon?:string
-  isDisables?:boolean
+  textStyles?: string;
+  rightIcon?: string;
+  isDisables?: boolean;
 }
 
 export interface SearchManufacturerProps {
@@ -29,12 +29,19 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
-export interface FilterProps{
-  manufacturer:string
-    year:number
-    fuel:string
-    limit:number
-    model:string
-  
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+export interface OptionProps {
+  title: string;
+  value: string;
 }
 
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
